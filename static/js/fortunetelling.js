0d6e59d9c43f2add52bad74204976320ff88239f
doyeonKim <0915dy@gmail.com>
2023-01-17 03:09:57 +0900

    feat : 운세 페이지 정보 화면 출력 #26
    `fortune`DB에서 띠 별 운세 정보를 가져와 화면에 출력한다.

--- static/js/fortunetelling.js
+++ static/js/fortunetelling.js
@@ -9,7 +9,6 @@ function show_fortunetelling() {
         data: {},
         success: function (response) {
             let rows = response['allFortune']
-            console.log(rows)
             for (let i = 0; i < rows.length; i++) {
                 let totalText = rows[i]['totalText']
                 let year1 = rows[i]['yearsInfo'][0]['year']
@@ -23,11 +22,7 @@ function show_fortunetelling() {
                 let year5 = rows[i]['yearsInfo'][4]['year']
                 let year5_text = rows[i]['yearsInfo'][4][' yearText']
 
-                console.log(year1, year1_text)
-                let html_temp = ``
-                switch (i) {
-                    case 0:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
+                let html_temp = `<div class="accordion-body">${totalText}</div>
                                      <ul>
                                         <li>${year1} : ${year1_text}</li>
                                         <li>${year2} : ${year2_text}</li>
@@ -35,127 +30,41 @@ function show_fortunetelling() {
                                         <li>${year4} : ${year4_text}</li>
                                         <li>${year5} : ${year5_text}</li>                                      
                                      </ul>`
+                switch (i) {
+                    case 0:
                         $('#flush-collapseOne').append(html_temp)
                         break;
                     case 1:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseTwo').append(html_temp)
                         break;
                     case 2:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseThree').append(html_temp)
                         break;
                     case 3:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseFour').append(html_temp)
                         break;
                     case 4:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseFive').append(html_temp)
                         break;
                     case 5:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseSix').append(html_temp)
                         break;
                     case 6:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseSeven').append(html_temp)
                         break;
                     case 7:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseEight').append(html_temp)
                         break;
                     case 8:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseNine').append(html_temp)
                         break;
                     case 9:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseTen').append(html_temp)
                         break;
                     case 10:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseEleven').append(html_temp)
                         break;
                     case 11:
-                        html_temp = `<div class="accordion-body">${totalText}</div>
-                                     <ul>
-                                        <li>${year1} : ${year1_text}</li>
-                                        <li>${year2} : ${year2_text}</li>
-                                        <li>${year3} : ${year3_text}</li>
-                                        <li>${year4} : ${year4_text}</li>
-                                        <li>${year5} : ${year5_text}</li>                                      
-                                     </ul>`
                         $('#flush-collapseTwelve').append(html_temp)
                         break;
                 }
