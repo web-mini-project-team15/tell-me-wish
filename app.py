e3851feac753277304fa0dfcfd46171551e7c81f
doyeonKim <0915dy@gmail.com>
2023-01-16 19:51:49 +0900

    fix : url 주소 수정 #15
    pull 받기 전에 커밋해서 다시 수정함

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import pymongo.errors
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-#
-# f = open('dbproperties', 'r', encoding="utf-8")
-# db_conn_info = f.readline()
-# client = MongoClient(db_conn_info)
-# db = client.playlist
+
+f = open('dbproperties', 'r', encoding="utf-8")
+db_conn_info = f.readline()
+client = MongoClient(db_conn_info)
+db = client.wish
 
 app = Flask(__name__)
 
@@ -18,6 +19,27 @@ def home():
 def wishRegisterPage():
     return render_template('index.html')
 
+# 소원 등록
+@app.route('/wish/regist', methods=['POST'])
+def wishSave():
+    nickName = request.form['nickname_give']
+    wish = request.form['wish_give']
+    if nickName == '' or wish == '':
+        return jsonify({'status' : '400'})
+    else :
+        try:
+            doc = {
+                'nickName' : nickName,
+                'wish' : wish,
+                'likeCount' : 0
+            }
+            db.wishes.insert_one(doc)
+            return jsonify({'status': '200'})
+        except :
+            return jsonify({'status': '500'})
+
+
+
 # 소원 보기 페이지
 @app.route('/wish-look-page', methods=["GET"])
 def wishLookPage():
@@ -27,6 +49,7 @@ def wishLookPage():
 @app.route('/fortunetelling-page', methods=["GET"])
 def fortunetellingPage():
     return render_template('fortunetelling.html')
+
 
 
 if __name__ == '__main__':
