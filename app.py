07986544b5f5a2f4731b58c59a0d949cf2ed9d69
doyeonKim <0915dy@gmail.com>
2023-01-16 15:10:00 +0900

    메인 상단 구조 잡기 및 전체 파일 구조 정리 #2

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-
-f = open('dbproperties', 'r', encoding="utf-8");
-db_conn_info = f.readline();
-client = MongoClient(db_conn_info)
-db = client.playlist
+#
+# f = open('dbproperties', 'r', encoding="utf-8")
+# db_conn_info = f.readline()
+# client = MongoClient(db_conn_info)
+# db = client.playlist
 
 app = Flask(__name__)
 
+# 메인 페이지 - 소원 빌기 페이지
 @app.route('/', methods=["GET"])
 def home():
     return render_template('index.html')
 
+# 소원 빌기 페이지
+@app.route('/wish/regist', methods=["GET"])
+def wishRegisterPage():
+    return render_template('index.html')
 
+# 소원 보기 페이지
+@app.route('/wish/look', methods=["GET"])
+def wishLookPage():
+    return render_template('wishLook.html')
+
+# 신년 운세 페이지
+@app.route('/fortunetelling', methods=["GET"])
+def fortunetellingPage():
+    return render_template('fortunetelling.html')
 
 
 if __name__ == '__main__':
